55574f53f1b3c04c7241e26f3641e1c872755495
sangwonme <paulmoguri@naver.com>
2022-08-26 00:40:46 +0900

    :sparkles: Add Flutter-Tutorial UI

--- flutter_tutorial_ui/lib/main.dart
+++ flutter_tutorial_ui/lib/main.dart
@@ -4,6 +4,53 @@ void main() {
   runApp(const MyApp());
 }
 
+class FavoriteWidget extends StatefulWidget {
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -32,11 +79,7 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red,
-          ),
-          Text('41')
+          FavoriteWidget(),
         ],
       ),
     );
